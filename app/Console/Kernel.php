1e7e08255e0fd78a9ce9884f7bd4ef6d2778dd53
Oleg Popkov <popkovoa@ektaservice.local>
2022-04-11 23:33:55 +0300

    finished cron task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\ImportsPostsDataCommand::class,
-        Commands\DemoCron::class,
+        Commands\ImportsPostsDataCommand::class
     ];
 
     /**
